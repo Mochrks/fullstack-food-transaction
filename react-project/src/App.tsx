231e6d8bd8ce1421cd232fd5d80d1edab95831b7
mochrks <mochrizkiks@gmail.com>
2024-09-30 20:26:06 +0700

    first commit

---
+++ react-project/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Button>Test</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
